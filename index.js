d4e7dc05215e304b5546f9f2334cff41fbff85e9
GOndronggg <andreamarco1212@gmail.com>
2020-12-18 15:27:39 +0700

    praktekdulu

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ app.get('/', function(req, res){
   res.send('Hello My name is Mas Mas Gondrong');
 });
 
+app.post('/', function(req, res){
+  res.send('Hello My name is Mas Mas Gondrong but its from post');
+});
+
+app.put('/', function(req, res){
+  res.send('Hello My name is Mas Mas Gondrong from put too');
+});
+
+app.patch('/', function(req, res){
+  res.send('Hello My name is Mas Mas Gondrong from patch');
+});
+
+app.delete('/', function(req, res){
+  res.send('Hello My name is Mas Mas Gondrong from delete');
+});
+
 app.listen(port, function(){
   console.log('Hello my apps is running on port '+port);
 });
+
